3850750a437b8986e6ec88aec936dd4909222f9d
Marc Daaboul <daaboulmarc@gmail.com>
2021-07-27 16:49:50 -0400

    fixed a possible crash

--- Script/Tree/editor/itemLists/itemList.gd
+++ Script/Tree/editor/itemLists/itemList.gd
@@ -3,7 +3,8 @@ extends ItemList
 # allows drag and drop functionality
 func get_drag_data(_pos):
 	var selected = get_selected_items()
-	var prev = TextureRect.new()
-	prev.texture = get_item_icon(selected[0])
-	set_drag_preview(prev)
-	return get_item_text(selected[0])
+	if selected.size() != 0:
+		var prev = TextureRect.new()
+		prev.texture = get_item_icon(selected[0])
+		set_drag_preview(prev)
+		return get_item_text(selected[0])
